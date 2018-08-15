d248889972daa94e8a32b84a997878f16a87d48d
Santiago Torres Alonso - Ceiba Software <santiago.torres@ceiba.com.co>
2018-08-15 10:03:53 -0500

    Se crea la estructura del proyecto y se compienza a desarrolar servicios

--- src/main/java/com/ceiba/estacionamiento/estacionamiento/dto/ParqueaderoDTO.java
+++ src/main/java/com/ceiba/estacionamiento/estacionamiento/dto/ParqueaderoDTO.java
@@ -1,5 +1,65 @@
 package com.ceiba.estacionamiento.estacionamiento.dto;
 
 public class ParqueaderoDTO {
+	
+	private Long id;
+	
+	private String nombre;
+	
+	private Long telefono;
+	
+	private String direccion;
+
+	public ParqueaderoDTO(Long id, String nombre, Long telefono, String direccion) {
+		this.id = id;
+		this.nombre = nombre;
+		this.telefono = telefono;
+		this.direccion = direccion;
+	}
+
+	public ParqueaderoDTO() {
+		super();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public Long getTelefono() {
+		return telefono;
+	}
+
+	public void setTelefono(Long telefono) {
+		this.telefono = telefono;
+	}
+
+	public String getDireccion() {
+		return direccion;
+	}
+
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+
+	@Override
+	public String toString() {
+		return "ParqueaderoDTO [id=" + id + ", nombre=" + nombre + ", telefono=" + telefono + ", direccion=" + direccion
+				+ "]";
+	}
+	
+	
+	
 
 }
--- src/main/java/com/ceiba/estacionamiento/estacionamiento/dto/RegistroVigilanteDTO.java
+++ src/main/java/com/ceiba/estacionamiento/estacionamiento/dto/RegistroVigilanteDTO.java
@@ -1,5 +1,117 @@
 package com.ceiba.estacionamiento.estacionamiento.dto;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
+import com.ceiba.estacionamiento.estacionamiento.entity.Parqueadero;
+
 public class RegistroVigilanteDTO {
+
+
+		private Long id;
+
+		private String placa;
+		
+		private int cilindraje;
+		
+		private Date horaEntrada;
+		
+		private Date horaSalida;
+		
+		private Boolean estado;
+		
+		private BigDecimal totalServicio;
+		
+		private Parqueadero parqueadero;
+
+		public RegistroVigilanteDTO() {
+			super();
+		}
+		
+
+		public RegistroVigilanteDTO(Long id, String placa, int cilindraje, Date horaEntrada, Date horaSalida,
+				Boolean estado, BigDecimal totalServicio, Parqueadero parqueadero) {
+			this.id = id;
+			this.placa = placa;
+			this.cilindraje = cilindraje;
+			this.horaEntrada = horaEntrada;
+			this.horaSalida = horaSalida;
+			this.estado = estado;
+			this.totalServicio = totalServicio;
+			this.parqueadero = parqueadero;
+		}
+
+
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getPlaca() {
+			return placa;
+		}
+
+		public void setPlaca(String placa) {
+			this.placa = placa;
+		}
+
+		public int getCilindraje() {
+			return cilindraje;
+		}
+
+		public void setCilindraje(int cilindraje) {
+			this.cilindraje = cilindraje;
+		}
+
+		public Date getHoraEntrada() {
+			return horaEntrada;
+		}
+
+		public void setHoraEntrada(Date horaEntrada) {
+			this.horaEntrada = horaEntrada;
+		}
+
+		public Date getHoraSalida() {
+			return horaSalida;
+		}
+
+		public void setHoraSalida(Date horaSalida) {
+			this.horaSalida = horaSalida;
+		}
+
+		public Boolean getEstado() {
+			return estado;
+		}
+
+		public void setEstado(Boolean estado) {
+			this.estado = estado;
+		}
+
+		public BigDecimal getTotalServicio() {
+			return totalServicio;
+		}
+
+		public void setTotalServicio(BigDecimal totalServicio) {
+			this.totalServicio = totalServicio;
+		}
+		
+		
+		public Parqueadero getParqueadero() {
+			return parqueadero;
+		}
+
+		public void setParqueadero(Parqueadero parqueadero) {
+			this.parqueadero = parqueadero;
+		}
+
+		
+		
+		
+
+	
 
 }
